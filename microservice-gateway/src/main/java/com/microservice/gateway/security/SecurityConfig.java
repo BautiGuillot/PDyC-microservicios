8e8623e908b2bcf6c3017367936f078f1ce14451
Bautista Guillot <bautistaguillot@hotmail.com>
2024-08-07 20:28:43 -0300

    se acomodaron las reglas de seguridad del gateway y de user

--- microservice-gateway/src/main/java/com/microservice/gateway/security/SecurityConfig.java
+++ microservice-gateway/src/main/java/com/microservice/gateway/security/SecurityConfig.java
@@ -2,12 +2,17 @@ package com.microservice.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.time.Duration;
+import java.util.Arrays;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,7 +21,7 @@ public class SecurityConfig {
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeExchange(auth -> auth
                         .pathMatchers(HttpMethod.POST, "/login").permitAll()
@@ -28,5 +33,20 @@ public class SecurityConfig {
                         .anyExchange().authenticated()) // se establece que cualquier otra petición requiere autenticación
                 .addFilterAt(new JWTAuthorizationFilter(), SecurityWebFiltersOrder.AUTHENTICATION); // se añade el filtro de autorización JWT
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedHeaders(Arrays.asList("Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers", "Access-Control-Expose-Headers", "Authorization"));
+        configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("*")); // Permitir cualquier origen
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"));
+        configuration.setMaxAge(Duration.ZERO);
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
--- microservice-user/src/main/java/com/microservice/user/security/SecurityConfig.java
+++ microservice-user/src/main/java/com/microservice/user/security/SecurityConfig.java
@@ -12,12 +12,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.time.Duration;
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +33,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .userDetailsService(userDetailsService)
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST,"/login/**").permitAll()
                         .requestMatchers(HttpMethod.GET,"/users/**").permitAll()
                         .anyRequest().fullyAuthenticated())
                 .addFilter(new JWTAuthenticationFilter
@@ -49,20 +44,6 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() { // Configuración de CORS, se permite el acceso desde cualquier origen, se permiten los métodos GET, POST, OPTIONS, PUT y PATCH, se permiten las cabeceras Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers y Authorization, se exponen las cabeceras Access-Control-Allow-Origin y Access-Control-Allow-Credentials y se permite el uso de credenciales.
-        CorsConfiguration cc = new CorsConfiguration();
-        cc.setAllowedHeaders(Arrays.asList("Origin", "Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers","Access-Control-Expose-Headers","Authorization"));
-        cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
-        cc.setAllowedOriginPatterns(Arrays.asList("*")); // Cambiado de setAllowedOrigins a setAllowedOriginPatterns
-        cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT","PATCH", "DELETE"));
-        cc.setMaxAge(Duration.ZERO);
-        cc.setAllowCredentials(Boolean.TRUE);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", cc);
-        return source;
     }
 
     public static void main(String[] args) {
